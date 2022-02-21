0d79cc63cb6ca494e1232301b45ec8c3681a47d6
black-tul1p <divay7@gmail.com>
2022-02-21 00:03:37 -0500

    Reworked code for wordler.py and updated wordlist

--- extract-words.py
+++ extract-words.py
@@ -3,7 +3,7 @@
 word_len = int(input("Enter the length of the words to be extracted: "))
 
 input_f = "words_alpha.txt"
-output_f = "wordle-words.txt"
+output_f = "wordle_words.txt"
 
 with open(input_f, "r") as input:
 	with open(output_f, "w") as output:
--- wordler.py
+++ wordler.py
@@ -11,7 +11,7 @@
 ########################################################################
 
 #############################  IMPORTS  ################################
-import enum
+import enum, random
 
 #############################  CLASSES  ################################
 
@@ -55,6 +55,7 @@ def get_score(word, guess):
 	score = []
 	maybe = collections.Counter(w for w, g in zip(word, guess) if w != g)
 
+	# Calculate score for guess against word
 	for guess_char, word_char in zip(guess, word):
 		if guess_char == word_char:
 			score.append(Let_Type.CORRECT)
@@ -68,50 +69,47 @@ def get_score(word, guess):
 
 
 ##############################  FUNCTION  ##############################
-# - Inputs  : None																									   #
-# - Outputs : None																									   #
+# - Inputs  : List of possible words																   #
+# - Outputs : Final list of words																		   #
 # - Desc		: A function that takes user input for guesses in a loop 	 #
 ########################################################################
-def guess_loop():
-	for _ in range(6):
+def play(wordlist):
+	while len(wordlist) > 1:
+		counter = 0
+		guess = get_word(wordlist)
+
 		# Get and store guess in array
-		guess = ""
-		while len(guess) != 10:
-			guess = input("Enter the "+ guess_num[_]+ " guess: ")
-			if (len(guess) != 10):
-				print("[X] Please enter the guess in the correct format\n")
-		guess = guess.lower().strip()
-		guesses.append(guess)
-
-		# Extract information from current guess
-		curr_probs, curr_chars = extract_info(guess)
-
+		score = ""
+		while len(score) != 5:
+			score = input("Enter the score for the "+ guess_num[counter]+ " guess: ")
+			if (len(score) != 5):
+				print("[X] Please enter the score in the correct format\n")
+		score_array = [x for x in score]
+		wordlist = update_list(words, guess, score_array)
 		print()
-		#print(guesses)
+		counter += 1
+	
+	return wordlist
+
 
 ##############################  FUNCTION  ##############################
 # - Inputs  : The user-entered guess string													   #
 # - Outputs : Arrays containing letters and probability from guesses   #
 # - Desc		: A function that extracts information from guesses				 #
 ########################################################################
-def extract_info(guess):
-	curr_lett = []
-	curr_prob = []
-	for i in range(len(guess)):
-		if (i+1) % 2 != 0:
-			curr_lett.append(guess[i])
-		else:
-			curr_prob.append(int(guess[i]))	
-	letters.append(curr_lett)
-	probs.append(curr_prob)
-	return curr_prob, curr_lett
+def get_word(wordlist):
+	print(f"I foresee {len(wordlist)} possibilities...")
+	guess = random.choice(wordlist)
+	print(f"Tempt fate with {guess!r}...")
+	return guess
+
 
 ##############################  FUNCTION  ##############################
 # - Inputs  : The user-entered guess string													   #
 # - Outputs : Arrays containing letters and probability from guesses   #
 # - Desc		: A function that extracts information from guesses				 #
 ########################################################################
-def get_list(words, guess, score):
+def update_list(words, guess, score):
 	possible_words = []
 
 	for word in words:
@@ -126,20 +124,27 @@ def get_list(words, guess, score):
 				if not maybe[guess_char]:
 					break
 				maybe[guess_char] -= 1
-			elif value = Let_Type.INCORRECT and maybe[guess_char]:
+			elif value == Let_Type.INCORRECT and maybe[guess_char]:
 				break
 		else:
 			possible_words.append(word)
 
 	return possible_words
 
+
+########################################################################
+
 if __name__ == '__main__':
 	with open(wordfile, "r") as file:
-		wordlist = [word.strip for word in file.readlines()]
+		wordlist = [word.strip().upper() for word in file.readlines()]
 
 	option = input("Enter \"h\" for help or \"p\" to play: ")
 
-	if option.strip.lower() == "h":
+	if option.strip().lower() == "h":
 		print(help_text)
 	else: 
 		words = play(wordlist)
+
+	if not words:
+		raise RuntimeError("No words can save you now.")
+	print(f"The only word that can end this is {words[0]!r}.")
